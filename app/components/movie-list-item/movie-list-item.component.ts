f1aca109404f3f7eac35e7726fefe5ac1695c170
Matt Farmer <work@mattfarmer.net>
2016-01-29 13:05:45 -0800

    Format the runtime

--- app/components/movie-list-item/movie-list-item.component.ts
+++ app/components/movie-list-item/movie-list-item.component.ts
@@ -13,4 +13,26 @@ import {Movie} from 'app/interfaces/movie';
 })
 export class MovieListItemComponent implements OnInit {
     public movie: Movie;
+
+    /**
+     * Convert movie.run_time to a formated string.
+     *
+     * Ideally this would be a Pipe, but given the time constraint is done here
+     */
+    getFormatedRuntime() {
+        let run_time = parseInt(this.movie.run_time, 10);
+        const SECS_IN_HOUR = 60*60;
+        const SECS_IN_MIN = 60;
+
+        if (isNaN(run_time)) {
+            return 'unknown';
+        }
+
+        let output = `${Math.round(run_time / SECS_IN_HOUR)}hrs`;
+        let minutes = Math.round(run_time % SECS_IN_HOUR) / SECS_IN_MIN;
+        if (0 !== minutes) {
+            output += ` ${Math.round(minutes % 60)}mins`;
+        }
+        return output;
+    }
 }
