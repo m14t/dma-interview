79bc7c2f8e9dcf14e12c36a17a0fdaf0caa2f3df
Matt Farmer <work@mattfarmer.net>
2016-01-31 21:30:00 -0800

    Convert MovieListItemComponent.getFormatedRuntime to a Pipe

--- app/components/movie-list-item/movie-list-item.component.ts
+++ app/components/movie-list-item/movie-list-item.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {Movie} from 'app/interfaces/movie';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-
+import {TimeFormatPipe} from 'app/pipes/time-format.pipe';
 
 @Component({
     directives: [
@@ -10,6 +10,7 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
     inputs: [
         'movie',
     ],
+    pipes: [TimeFormatPipe],
     selector: 'movie-list-item',
     styleUrls: [
         'app/components/movie-list-item/movie-list-item.component.css',
@@ -18,26 +19,4 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 })
 export class MovieListItemComponent implements OnInit {
     public movie: Movie;
-
-    /**
-     * Convert movie.run_time to a formated string.
-     *
-     * Ideally this would be a Pipe, but given the time constraint is done here
-     */
-    getFormatedRuntime() {
-        let run_time = parseInt(this.movie.run_time, 10);
-        const SECS_IN_HOUR = 60*60;
-        const SECS_IN_MIN = 60;
-
-        if (isNaN(run_time)) {
-            return 'unknown';
-        }
-
-        let output = `${Math.round(run_time / SECS_IN_HOUR)}hrs`;
-        let minutes = Math.round(run_time % SECS_IN_HOUR) / SECS_IN_MIN;
-        if (0 !== minutes) {
-            output += ` ${Math.round(minutes % 60)}mins`;
-        }
-        return output;
-    }
 }
---
+++ app/pipes/time-format.pipe.ts
@@ -0,0 +1,31 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+
+const SECS_IN_HOUR = 60 * 60;
+const SECS_IN_MIN = 60;
+
+/**
+ * Takes a number of seconds, and converts it to a string like
+ * "1hrs 2min" (or "14secs" if the time is less than 1 minute).
+ */
+@Pipe({name: 'timeFormat'})
+export class TimeFormatPipe implements PipeTransform {
+    transform(seconds: number): string {
+        let output = [];
+
+        let hours = Math.floor(seconds / SECS_IN_HOUR);
+        if (0 !== hours) {
+            output.push(`${hours}hrs`);
+        }
+
+        let minutes = Math.floor((seconds % SECS_IN_HOUR) / SECS_IN_MIN);
+        if (0 !== minutes) {
+            output.push(`${minutes}mins`);
+        }
+
+        if (0 === output.length) {
+            output.push(`${seconds}secs`);
+        }
+
+        return output.join(' ');
+    }
+}
