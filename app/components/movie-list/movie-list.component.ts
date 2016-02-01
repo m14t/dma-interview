ec486ebbb19e25dc73be2e664e252b68cd695381
Matt Farmer <work@mattfarmer.net>
2016-02-01 15:27:39 -0800

    Alphabetize import statements

--- app/components/movie-list/movie-list.component.ts
+++ app/components/movie-list/movie-list.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {MovieListItemComponent} from '../movie-list-item/movie-list-item.component';
 import {MovieService} from '../../services/movie.service';
 import {Movie} from '../../interfaces/movie';
-import {MovieListItemComponent} from '../movie-list-item/movie-list-item.component';
 
 const ASC = 'ASC';
 const DESC = 'DESC';
